2b0ee3163de6a3ba379b7358180876718a97de74
Wafa-Ag <prsscontact@gmail.com>
2023-02-11 19:43:05 +0100

    c4 register_menus , add_theme_support , création d'un lien pour le titre principale du site

--- header.php
+++ header.php
@@ -8,9 +8,11 @@
     <?php wp_head(); ?>
 </head>
 <body>
-    <header>
+    <header>    
+        <h1> <a href= "<?php  bloginfo('name'); ?>"> </a> </h1>
         <h1><?php  bloginfo('name'); ?></h1>
         <h2><?php  bloginfo('description'); ?></h2>
         <h2><?php  bloginfo('url'); ?></h2>
+        <h1><a href="<?php  bloginfo('url'); ?>"><?php  bloginfo('name'); ?></a></h1>
     </header>
     
