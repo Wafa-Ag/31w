e24feae3ec0dceef781a8aa477a99ad614f8d32d
Wafa-Ag <prsscontact@gmail.com>
2023-02-23 02:45:48 +0100

    insertion la base de données dans le cours.sql , ajouter la fonction qui permet de modifier la requette pricipale de WordPresse dans function.php

--- front-page.php
+++ front-page.php
@@ -1,29 +1,25 @@
-<?php
-/**
-    Modèle index.php représente le modèle par défaut du thème
-*/
-get_header() ?>
+<!-- modele index.php représente le modele par défaut  -->
+<?php get_header(); ?><!-- Il va récuperer le header -->
 <main>
-    <code><frond-page class="php"></frond-page> </code>
+    <code>front-page.php</code>
     <h3>index.php</h3>
- <session class="blocflex"> 
-<?php 
-if (have_posts()):
-    while (have_posts()) : the_post();
-        //the_title('<h1>','</h1>');
-       // the_permalink();?>
-       <article> 
-       <h3> <a href ="<?php the_permalink();?>"> <?= get_the_title(); ?></a> </h3>
-        </article>
-        <?php 
-        //the_content();
-        // the_content(); 
-        //the_excerpt();
-    echo wp_trim_words(get_the_excerpt(), 4);
-    endwhile;
-endif;
-?>  
-</session> 
-</main> 
-<?php get_footer(); ?>
+    <section class="blocflex">
+        <?php if (have_posts()) :
+            while (have_posts()) : the_post();
+                //the_title('<h1>', '</h1>');
+                //the_permalink();
+                /* ============== */
+                //the_content();
+                //the_excerpt();
+        ?>
+                <article>
+                    <h3><a target=_blank href="<?php the_permalink(); ?>"><?= get_the_title(); ?></a></h3>
 
+                    <p><?php echo wp_trim_words(get_the_excerpt(), 4); ?></p> <!-- Le nombre de mot que je veux après la premiere virgule, le troisieme arguement est optionnel  -->
+                </article>
+            <?php endwhile; ?>
+        <?php endif;
+        ?>
+    </section>
+</main>
+<?php get_footer(); ?>
