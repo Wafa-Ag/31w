1f4887eaa00203f40fb4a75912099aff2dc6cca7
Wafa-Ag <prsscontact@gmail.com>
2023-03-02 22:37:09 +0100

    c10  ajouter un  dosier template-parts qui contient categorie-galerie.php

--- front-page.php
+++ front-page.php
@@ -6,14 +6,15 @@
     <section class="blocflex">
         <?php if (have_posts()) :
             while (have_posts()) : the_post();
-                //the_title('<h1>', '</h1>');
-                //the_permalink();
-                /* ============== */
-                //the_content();
-                //the_excerpt();
+
+            if (in_category('galerie'))
+            {
+                get_template_part ("template-parts/categorie", "galerie");
+            } else{
+                get_template_part ("template-parts/categorie", "note-wp");
+            }
+
         ?>
-                
-                <?php get_template_part ("template-parts/categorie", "note-wp"); ?>
             <?php endwhile; ?>
         <?php endif;
         ?>
---
+++ template-parts/categorie-galerie.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * templat-part qui permettra d'affichier un article provenat d'un conteneur de classe  blocflex 
+ * pour un article de catégoriz galerie
+ */
+
+?>
+<article class="blocflex_galerie">
+               <?php the_content(); ?> 
+</article>
+            
