e21cbaccac04ba7e50be8d1a4915a080ac75403c
Wafa-Ag <prsscontact@gmail.com>
2023-03-26 16:43:33 +0100

    c16 : formatage de menu et background dans _blocflex.scss , menu aside dans _menu_cours.scss

--- header.php
+++ header.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta author="X to the Z" >
     <title>Document</title>
     <?php wp_head(); ?>
 </head>
@@ -20,15 +19,16 @@
                 )); ?> 
                 <?= get_search_form(); ?>
                 <label for="chkBurger" class="burger">
-                <img src="https://s2.svgbox.net/hero-outline.svg?ic=menu&color=FFFFFF" width="32" height="32">
-                </label> 
-                <label for="chkBurger" class="fermer">
-                <img src="https://s2.svgbox.net/materialui.svg?ic=close&color=FFFFFF" width="32" height="32">
-                </label>  
+                <img src="https://s2.svgbox.net/hero-outline.svg?ic=menu&color=000" width="32" height="32">
+                </label>   
             </div> 
         </section>
-        <h1><a href="<?php  bloginfo('url'); ?>"><?php  bloginfo('name'); ?></a></h1> 
-        <h2><?php  bloginfo('description'); ?></h2>
+        <?php 
+        $classe = "";
+        if (is_front_page() == false){$classe="invisible";}
+        ?>
+        <h1 class="site__titre <?= $classe ?>"><a href="<?php  bloginfo('url'); ?>"><?php  bloginfo('name'); ?></a></h1> 
+        <h2 class="site__soustitre  <?= $classe ?>"><?php  bloginfo('description'); ?></h2>
     </header>
     <?php 
     if (is_front_page() == false)
@@ -36,3 +36,4 @@
        get_template_part("template-parts/aside");
     }
      ?>
+    
--- single.php
+++ single.php
@@ -1,17 +1,15 @@
 <?php
 /**
-    Modèle index.php représente le modèle par défaut du thème
+* Modèle index.php représente le modèle par défaut du thème
 */
 get_header() ?>
 <main class="site__main">
-    <h3>Single.php</h3>
+    <h3>single.php</h3>
 <?php 
 if (have_posts()):
    while(have_posts()): the_post();
         the_title('<h1>','</h1>');
-        the_content(); 
-        /* in_category() */
-        the_field('enseignant');
+        the_content();  
     endwhile;    
 endif;
 ?>   
